87d099fbfb658f77f6db7799620e0cbebaa541c2
sharmila1214 <sweety.1428@gmail.com>
2020-04-14 00:19:26 -0700

    first

---
+++ gir_demo_repos.java
@@ -0,0 +1,10 @@
+package git_demo;
+
+public class gir_demo_repos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is demo for the git");
+	}
+
+}
