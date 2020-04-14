a11e0f2fd3c8d20a1180e1e5c5808d8e769d253e
sharmila1214 <sweety.1428@gmail.com>
2020-04-14 00:51:38 -0700

    second commit

--- gir_demo_repos.java
+++ gir_demo_repos.java
@@ -5,6 +5,8 @@ public class gir_demo_repos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is demo for the git");
+System.out.println("succefully push my code into github");
+System.out.println("second commit");
 	}
 
 }
